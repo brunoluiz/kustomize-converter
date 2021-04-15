d2c612e27cb90e1ead55e454165c80d5648f8a2a
Bruno Luiz Silva <contact@brunoluiz.net>
2021-04-15 15:44:36 +0100

    chore: use urfave

--- cmd/kustomize-converter/main.go
+++ cmd/kustomize-converter/main.go
@@ -1,50 +1,49 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/brunoluiz/kustomize-converter/internal/kustomize"
 	"github.com/brunoluiz/kustomize-converter/internal/loader"
 	"github.com/brunoluiz/kustomize-converter/internal/writer"
-	"github.com/peterbourgon/ff/v3"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fs := flag.NewFlagSet("kustomize-converter", flag.ExitOnError)
-
-	var (
-		folder           = fs.String("folder", "", "kubernetes manifests source folder")
-		outputFolder     = fs.String("output-folder", "", "kubernetes manifest output folder (can be the same as --folder)")
-		clean            = fs.Bool("clean", false, "if set to true, it will clear up resources from source folder before generating output")
-		enableGenerators = fs.Bool("generators", true, "if set to false, disable secret and configMapGenerator transforms")
-		namespace        = fs.String("namespace", "", "kubernetes namespace for this resources")
-		configsFolder    = fs.String("configs-folder", "configs", "which folder should the ConfigMaps be placed in the output folder")
-		secretsFolder    = fs.String("secrets-folder", "secrets", "which folder should the Secrets be placed in the output folder")
-	)
-
-	if err := ff.Parse(fs, os.Args[1:], ff.WithEnvVarNoPrefix()); err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{Name: "folder", Usage: "kubernetes manifests source folder", Required: true},
+		&cli.StringFlag{Name: "output-folder", Usage: "kubernetes manifest output folder (can be the same as --folder)", Required: true},
+		&cli.StringFlag{Name: "namespace", Usage: "kubernetes namespace for this resources", Required: true},
+		&cli.StringFlag{Name: "secrets-folder", Usage: "which folder should the ConfigMaps be placed in the output folder", Value: "secrets"},
+		&cli.StringFlag{Name: "configs-folder", Usage: "which folder should the Secrets be placed in the output folder", Value: "configs"},
+		&cli.BoolFlag{Name: "clean", Usage: "if set to true, it will clear up resources from source folder before generating output", Value: false},
+		&cli.BoolFlag{Name: "generators", Usage: "if set to false, disable secret and configMapGenerator transforms", Value: true},
 	}
 
-	if *namespace == "" {
-		log.Fatal("Missing namespace details")
+	app.Action = func(c *cli.Context) error {
+		k, err := loader.FromFS(c.String("folder"),
+			kustomize.WithBaseFolder(c.String("folder")),
+			kustomize.WithNamespace(c.String("namespace")),
+			kustomize.WithConfigsFolder(c.String("configs-folder")),
+			kustomize.WithSecretsFolder(c.String("secrets-folder")),
+			kustomize.WithGenerators(c.Bool("generators")),
+			kustomize.WithProcessedLog(c.Bool("clean")),
+		)
+		if err != nil {
+			return errors.Wrap(err, "issue on loading kubernetes manifests")
+		}
+
+		if err := writer.ToFS(c.String("output-folder"), c.Bool("clean")).Write(k); err != nil {
+			return errors.Wrap(err, "issue on writing kubernetes manifests")
+		}
+
+		return nil
 	}
 
-	k, err := loader.FromFS(*folder,
-		kustomize.WithBaseFolder(*folder),
-		kustomize.WithGenerators(*enableGenerators),
-		kustomize.WithNamespace(*namespace),
-		kustomize.WithProcessedLog(*clean),
-		kustomize.WithConfigsFolder(*configsFolder),
-		kustomize.WithSecretsFolder(*secretsFolder),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := writer.ToFS(*outputFolder, *clean).Write(k); err != nil {
-		log.Fatal(err)
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err.Error())
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/brunoluiz/kustomize-converter
 go 1.16
 
 require (
-	github.com/peterbourgon/ff/v3 v3.0.0
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.20.5
 	k8s.io/apimachinery v0.20.5
